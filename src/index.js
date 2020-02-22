1d2f5de103f1a69901a26f3854e38a132a72e37a
ArtyomBlr <59910600+ArtyomBlr@users.noreply.github.com>
2020-02-22 15:45:10 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) == false || array == undefined){
+    return 0;
+  } else if (array.length == 0) {
+    return 0;
+  } else {
+    return Math.min.apply(null, array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) == false || array == undefined){
+    return 0;
+  } else if (array.length == 0) {
+    return 0;
+  } else {
+    return Math.max.apply(null, array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) == false || array == undefined){
+    return 0;
+  } else if (array.length == 0) {
+    return 0;
+  } else {
+    var a = 0;
+    for(var i = 0; i < array.length; i++) {
+       a += array[i]/array.length;
+    }
+    return  (Math.round((a)*100)/100);  
+  }
 }
